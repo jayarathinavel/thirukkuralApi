24d5b703106c53ab6b2ec1d49ec1b7c7baa18e11
jayarathinavel <jayrathinavel@gmail.com>
2021-12-07 21:40:03 +0530

    First Commit

---
+++ src/main/java/com/hobbyproject/thirukkuralapi/ThirukkuralApiApplication.java
@@ -0,0 +1,13 @@
+package com.hobbyproject.thirukkuralapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirukkuralApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirukkuralApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobbyproject/thirukkuralapi/ThirukkuralApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyproject.thirukkuralapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirukkuralApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
